5ba6f7378955d0ba75c9fbc38d2390ca2d4540fa
Hnybon <43375200+Hnybon@users.noreply.github.com>
2021-01-21 13:14:18 +0100

    Search og Authorize

--- Models/Address.cs
+++ Models/Address.cs
@@ -27,15 +27,19 @@ namespace BeckITEjendomsmæglerApplikation.Models
         public DateTime DateOfInsertion { get { return CalculateStartLiggetid(); } set {  } }
         public int? Liggetid { get { return CalculateLiggetid(); } set { } }
 
-        public bool Villa { get; set; }
-        public bool Ejerlejlighed { get; set; }
-        public bool Andelsbolig { get; set; }
-        public bool Helårsgrund { get; set; }
-        public bool Villalejlighed { get; set; }
-        public bool Rækkehus { get; set; }
-        public bool Fritidsbolig { get; set; }
-        public bool Landejendom { get; set; }
-        public bool Fritidsgrund { get; set; }
+        public int BoligTypeID { get; set; }
+        [ForeignKey("BoligTypeID")]
+        public BoligType Type { get; set; }
+
+        //public bool Villa { get; set; }
+        //public bool Ejerlejlighed { get; set; }
+        //public bool Andelsbolig { get; set; }
+        //public bool Helårsgrund { get; set; }
+        //public bool Villalejlighed { get; set; }
+        //public bool Rækkehus { get; set; }
+        //public bool Fritidsbolig { get; set; }
+        //public bool Landejendom { get; set; }
+        //public bool Fritidsgrund { get; set; }
 
         public DateTime CalculateStartLiggetid()
         {
